b6bbba02b788dd69f9583bf1ebba1945206f2a29
Daniel Parucci Somerlate <89330408+Paruccikk@users.noreply.github.com>
2024-06-23 16:01:08 -0300

    Update set_webhook.php

--- set_webhook.php
+++ set_webhook.php
@@ -4,7 +4,7 @@ $botToken = '7225700924:AAFQbefX9Tc2eR4xQxRSU_M-7vmIHb87Ohk';
 $apiUrl = "https://api.telegram.org/bot$botToken/";
 
 // Substitua 'your-vercel-domain' pelo seu domínio no Vercel
-$webhookUrl = 'https://your-vercel-domain.vercel.app/api/bot.php';
+$webhookUrl = 'https://contador-bot.vercel.app/bot.php';
 
 $setWebhookUrl = $apiUrl . "setWebhook?url=" . $webhookUrl;
 
